19ab1f5bd8b8b96961360784164df3c68f6161dd
farizl <farizl@handsv.com>
2026-08-14 16:45:08 +0700

    update migration

---
+++ Migrations/20260814091619_AddLicenseTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LicenseService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLicenseTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
